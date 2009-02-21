abc7835dd5df1385f29346f7eab77053b3c8dd01
lp <lp@spiralix.org>
2009-02-21 18:57:11 -0500

    pops when no server defined

--- lib/greetings.rb
+++ lib/greetings.rb
@@ -1,11 +1,16 @@
 module AmuseHelpers
 	
 	def greetings
-		layout do
-			button("START", :margin => 10) { clear; dashboard }
+		if $conf[:server].nil?
+			alert("Please define your server before the fun starts!!")
+			clear; admin
+		else
+			layout do
+				button("START", :margin => 10) { clear; dashboard }
+			end
+			@title.replace "Bienvenido!!"
+			@display.replace "Welcome to Amuse!!!"
 		end
-		@title.replace "Bienvenido!!"
-		@display.replace "Welcome to Amuse!!!"
 	end
 	
 end
