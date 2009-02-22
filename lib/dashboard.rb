4be91bd35be5678b24eb529c82a2c17f0c0db6b2
lp <lp@spiralix.org>
2009-02-22 08:36:36 -0500

    dashboard has basic project functionnalities

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -2,16 +2,46 @@ module AmuseHelpers
 	
 	def dashboard
 		@dashboard = serial_read(:dashboard)
+		@projects = serial_read(:projects)
 		layout do
+			stack do
+				colorContent('Projects:', white)
+				unless @projects.first.empty?
+					flow do
+						colorContent('Name:', white)
+						colorContent('Description:', white)
+					end
+					@projects.each do |p|
+						flow do
+							para( p[:name],
+			          :size => 12, 
+			          :align => "Left", 
+			          :font => "Trebuchet MS", 
+			          :stroke => red
+			        )
+							para( p[:description],
+			          :size => 10, 
+			          :align => "left", 
+			          :font => "Trebuchet MS", 
+			          :stroke => blue
+			        )
+							button("Go!", :margin => 10) { @current_project = p[:id]; clear; messages }
+						end
+					end
+				end
+				button("New Project", :margin => 10) { clear; projects }
+			end
 			stack do
 				unless @dashboard.first.empty?
 					@dashboard.each do |t|
+						colorContent('Title:', white)
 						para( t[:title],
 		          :size => 14, 
 		          :align => "Left", 
 		          :font => "Trebuchet MS", 
 		          :stroke => red
 		        )
+						colorContent('Message:', white)
 						para( t[:message],
 		          :size => 12, 
 		          :align => "left", 
@@ -28,7 +58,6 @@ module AmuseHelpers
 			end
 		end
 		@title.replace "Dashboard"
-		@display.replace "Recent Messages:"
 	end
 	
 end
