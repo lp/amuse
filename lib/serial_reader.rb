d1382a6a8ac038f4726534fb9e25792ae5c31b98
lp <lp@spiralix.org>
2009-02-21 21:43:13 -0500

    add one more id in path

--- lib/serial_reader.rb
+++ lib/serial_reader.rb
@@ -3,12 +3,12 @@ module AmuseHelpers
 	require 'net/http'
 	require 'uri'
 	
-	def serial_read(grade,id='')
+	def serial_read(grade,id1='',id2='')
 		@display.replace 'Fetching cached data '
 		YAML::load(
 			decrypt(
 				Net::HTTP.get(
-					URI.parse("http://#{$conf[:server]}/#{$server[grade][:r]}#{id}.cache")
+					URI.parse("http://#{$conf[:server]}/#{$server[grade][:r]}#{id1}#{id2}.cache")
 				).chomp))
 	end
 
