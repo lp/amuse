45562f9a48d20f9352fcbabe83f13c520ef730b1
lp <lp@spiralix.org>
2009-02-23 09:29:29 -0500

    temp layout to test functionalities

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,8 +1,30 @@
 module AmuseHelpers
 	
 	def messages
-		thread = serial_read(:messages,"#{@current_project.to_s}/",1)
+		@current_thread = 1 if @current_thread.nil?
+		thread = serial_read(:messages,"#{@current_project.to_s}/",@current_thread)
+		@threads = serial_read(:threads,@current_project)
 		layout do
+			stack do
+				colorContent('Threads:', white)
+				unless @threads.first.empty?
+					flow do
+						colorContent('Subjects:', white)
+					end
+					@threads.each do |p|
+						flow do
+							para( p[:subject],
+			          :size => 12, 
+			          :align => "Left", 
+			          :font => "Trebuchet MS", 
+			          :stroke => red
+			        )
+							button("Go!", :margin => 10) { @current_thread = p[:id]; clear; messages }
+						end
+					end
+				end
+				button("New Thread", :margin => 10) { clear; threads }
+			end
 			stack do
 				unless thread.first.empty?
 					thread.each do |t|
@@ -27,7 +49,7 @@ module AmuseHelpers
 			end
 			button("Submit", :margin => 10) { store_message({:author_id => $conf[:author],
 																													:project_id => @current_project,
-																													:thread_id => 1,
+																													:thread_id => @current_thread,
 																													:title => @subject.text,
 																													:message => @message.text })
 																						clear; messages }
