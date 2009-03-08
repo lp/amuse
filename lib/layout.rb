27f70c8e21dac5e3dc132d28e91ec237bb46551b
lp <lp@spiralix.org>
2009-03-07 21:39:41 -0500

    layout scrolls

--- lib/layout.rb
+++ lib/layout.rb
@@ -1,8 +1,13 @@
 module AmuseHelpers
 	
-	def layout
-    background black
+	def layout(args={:scroll => false})
+		background lightslategray
 		stack do
+			stroke white
+			stack do
+				fill slategray
+				rect :top => 10, :left => 10, :width => 580, :height => 660, :curve => 40
+			end
     	stack(:margin => 10) do
 	      @title = title( '', 
 	        :align => "center",
@@ -19,12 +24,12 @@ module AmuseHelpers
 	        )
 			end
 		end
-		stack(:margin => 20) do
+		stack(:top => 40, :left => 40,:margin => 20,:height => 640,:width => 560,:scroll => args[:scroll]) do
       yield if block_given?
 			@quit = button('quit', :margin => 10) { exit }
     end
   end
-  
+	
   def colorContent(text,color)
     return para( text,
       :size => 12,
