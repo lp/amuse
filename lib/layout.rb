bad98aeb926fae813132d60c9b146e87e55eb047
lp <lp@spiralix.org>
2009-03-08 09:49:44 -0400

    sorted out the basic generic layout

--- lib/layout.rb
+++ lib/layout.rb
@@ -1,18 +1,28 @@
+# Author:: lp (mailto:lp@spiralix.org)
+# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+# 
+# :title:AmuseHelpers
 module AmuseHelpers
 	
 	def layout(args={:scroll => false})
+		args = layout_default(args)
 		background lightslategray
 		stack do
-			stroke white
 			stack do
-				fill slategray
-				rect :top => 10, :left => 10, :width => 580, :height => 660, :curve => 40
+				stroke white; fill black
+				rect :top => 10, :left => 10, :width => 580, :height => 100, :curve => 15
+				stroke black; fill gray
+				rect :top => 70, :left => 20, :width => 560, :height => 35, :curve => 5
 			end
-    	stack(:margin => 10) do
+			stack do
+				stroke white; fill slategray
+				rect :top => 120, :left => 10, :width => 580, :height => 560, :curve => 40
+			end
+    	stack(:margin => 8) do
 	      @title = title( '', 
 	        :align => "center",
 	        :font => "Trebuchet MS",
-	        :stroke => pink
+	        :stroke => white
 	      )
 	    end
 	    stack(:margin => 20) do
@@ -24,9 +34,8 @@ module AmuseHelpers
 	        )
 			end
 		end
-		stack(:top => 40, :left => 40,:margin => 20,:height => 640,:width => 560,:scroll => args[:scroll]) do
+		stack(:top => 130, :left => 20,:height => 560,:width => 560,:scroll => args[:scroll]) do
       yield if block_given?
-			@quit = button('quit', :margin => 10) { exit }
     end
   end
 	
@@ -39,5 +48,12 @@ module AmuseHelpers
       :stroke => color
     )
   end
+
+	private
+	
+	def layout_default(args)
+		args[:scroll] = false if args[:scroll].nil?
+		args
+	end
 	
 end
