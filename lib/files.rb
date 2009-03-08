00e462b6220c06308d247ed47837c968dd2e2220
lp <lp@spiralix.org>
2009-03-08 09:50:10 -0400

    cleanup and copyright

--- lib/files.rb
+++ lib/files.rb
@@ -1,3 +1,7 @@
+# Author:: lp (mailto:lp@spiralix.org)
+# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+# 
+# :title:AmuseHelpers
 module AmuseHelpers
 	require 'digest'
 	require 'httpclient'
@@ -6,7 +10,6 @@ module AmuseHelpers
 	
 	def files
 		layout do
-			# button("Download...", :margin => 10) { get_file }
 			button("Upload...", :margin => 10) { put_file }
 			button("Dashboard", :margin => 10) { clear; dashboard }
 		end
@@ -23,27 +26,17 @@ module AmuseHelpers
 	# 		end
 	# 	end
 	
-	# def put_file
-	# 							@display.replace "putting file..."
-	# 							uri = URI.parse('http://amuse.spiralix.org/app/files/new/big/bogus.pdf')
-	# 							Net::HTTP.start(uri.host,uri.port) do |http|
-	# 								req = Net::HTTP::Put.new("#{uri.path}")
-	# 								response = http.request( req, File.open('bogus.pdf','r').read)
-	# 								@display.replace "response code: #{response.code} message: #{response.message} body: #{response.body}"
-	# 							end
-	# 						end
-	
 	def put_file
-								uri = URI.parse('http://amuse.spiralix.org/upload/new/goo/bogus.pdf')
-								path = 'bogus.pdf'; block_size = 2097152; done = 0
-								digest = Digest::SHA1.digest(path)
-								File.open(path,'r') do |file|
-									while part = file.read(block_size)
-										done += 1; body = { 'upload' => part, 'part' => done, 'digest' => digest}
-									  HTTPClient.new.post_content(uri, body, {'Content-Type' => 'multipart/form-data'})
-									end
-								end
-								@display.replace "Done!!!"
-							end
+		uri = URI.parse('http://amuse.spiralix.org/upload/new/goo/bogus.pdf')
+		path = 'bogus.pdf'; block_size = 2097152; done = 0
+		digest = Digest::SHA1.digest(path)
+		File.open(path,'r') do |file|
+			while part = file.read(block_size)
+				done += 1; body = { 'upload' => part, 'part' => done, 'digest' => digest}
+			  HTTPClient.new.post_content(uri, body, {'Content-Type' => 'multipart/form-data'})
+			end
+		end
+		@display.replace "Done!!!"
+	end
 	
 end
