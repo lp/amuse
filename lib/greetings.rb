b5ea4e3d8f5d7d099d1bbca21a68cafb4b6ba01a
lp <lp@spiralix.org>
2009-03-08 15:50:57 -0400

    base login hook

--- lib/greetings.rb
+++ lib/greetings.rb
@@ -5,16 +5,58 @@
 module AmuseHelpers
 	
 	def greetings
+		login = Hash.new
 		if $conf[:server].nil?
 			alert("Please define your server before the fun starts!!")
 			clear; admin
 		else
 			layout(:scroll => true) do
-				stroke black; fill darkgray
-				rect :top => 120, :left => 110, :width => 340, :height => 260, :curve => 20
-				button("START", :top => 340, :left => 350) { clear; dashboard }
+				stack :top => 120, :left => 110 do
+					stroke black; fill darkgray
+					rect :width => 340, :height => 260, :curve => 20
+					stack :top => 40, :left => 30 do
+						stroke black; fill dimgray
+						rect :width => 280, :height => 40, :curve => 3
+						stack :top => 5, :left => 5 do
+							para 'Author: ', :size => 16, :font => "Trebuchet MS", :stroke => red
+						end
+						stack :top => 5, :left => 80 do
+							login[:author] = edit_line($conf[:author], :width => 180)
+						end
+					end
+					stack :top => 130, :left => 30 do
+						stroke black; fill dimgray
+						rect :width => 280, :height => 40, :curve => 3
+						stack :top => 5, :left => 5 do
+							para 'Server: ', :size => 16, :font => "Trebuchet MS", :stroke => red
+						end
+						stack :top => 5, :left => 80 do
+							login[:server] = edit_line($conf[:server], :width => 180)
+						end
+					end
+					button("START", :top => 220, :left => 240) { 	$conf[:author] = login[:author].text
+																												$conf[:server] = login[:server].text
+																												store_conf; clear; login}
+				end
 			end
 			@title.replace "Login"
+		end
+	end
+	
+	private
+	
+	def login
+		connect('greetings') do
+			key = Keys.temp
+			authors_list = serial_read('ipkey',key,:authors)
+			selected = authors_list.select { |a| $conf[:author] =~ /#{a[:name]}|#{a[:nickname]}/i}
+			if selected.empty?
+				alert("You need to register as an Author First!!!")
+				clear; authors
+			else
+				$runtime[:author] = selected.first
+				Keys.get; clear; dashboard
+			end
 		end
 	end
 	
